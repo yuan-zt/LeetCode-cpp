8940c4cb95e250f44a7a2e7c1772e70e24cb49d3
yuan-zt <55337992+yuan-zt@users.noreply.github.com>
2020-12-22 19:05:27 +0800

    Create Number of Islands.cpp

---
+++ leetcode/0200.Number of Islands/Number of Islands.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int nr = grid.size();
+        if (!nr) return 0;
+        int nc = grid[0].size();
+
+        int num_islands = 0;
+        for (int i = 0; i < nr; ++i) {
+            for (int j = 0; j < nc; ++j) {
+                if (grid[i][j] == '1') {
+                    ++num_islands;
+                    grid[i][j] = '0';
+                    queue<pair<int, int>> neighbors;
+                    neighbors.push({i, j});
+                    while (!neighbors.empty()) {
+                        auto rc = neighbors.front();
+                        neighbors.pop();
+                        int row = rc.first, col = rc.second;
+
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+
+                            neighbors.push({row-1, col});
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.push({row+1, col});
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.push({row, col-1});
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.push({row, col+1});
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+};
