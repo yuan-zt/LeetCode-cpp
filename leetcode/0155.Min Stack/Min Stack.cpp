567a984b32131638f37b5970324c727fee25c059
yuan-zt <55337992+yuan-zt@users.noreply.github.com>
2020-12-23 19:23:23 +0800

    Create Min Stack.cpp

---
+++ leetcode/0155.Min Stack/Min Stack.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<int> min_stack;
+    stack<int> x_stack;
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+        min_stack.push(INT_MAX);
+    }
+    
+    void push(int x) {
+        x_stack.push(x);
+        min_stack.push(min(min_stack.top(),x));
+    }
+    
+    void pop() {
+        x_stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return x_stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
